7c35c1600d52e8449a56bd57b5e537073a388ea5
Nahuel <naguieta@gmail.com>
2025-08-06 20:27:24 -0300

    modify dotenv

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 import os
-from dotenv import load_dotenv
+from .env import load_dotenv
 
 load_dotenv()
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
